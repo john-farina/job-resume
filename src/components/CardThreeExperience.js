5d91120575b239eaa5a24aa7e88ce79cb208a42c
onefootfarina <clichecontent@gmail.com>
2022-09-20 21:32:45 -0400

    nice experience

--- src/components/CardThreeExperience.js
+++ src/components/CardThreeExperience.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
 function CardThreeExperience() {
+  let string = '"${yourCompanyName}"';
+  let [popUpStyle, setPopUpStyle] = useState({
+    display: "none",
+  });
+
   return (
     <div id="card3" className="card">
       <div className="cardBack cardBack3"></div>
+      <div style={popUpStyle} className="alertPopUp">
+        <p className="cardText">TypeError: yourCompanyName is undefined</p>
+      </div>
       <div className="cardContents">
         <h2 className="cardTitle">Experience:</h2>
-        <p className="cardText">- Nothing Yet</p>
+        <p className="cardText">
+          -{" "}
+          <span
+            onMouseEnter={() => {
+              setPopUpStyle({ display: "flex" });
+            }}
+            onMouseLeave={() => {
+              setPopUpStyle({ display: "none" });
+            }}
+            class="companyNameText"
+          >
+            {string}
+          </span>
+        </p>
         <p className="cardText"></p>
       </div>
     </div>
