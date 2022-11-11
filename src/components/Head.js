3bf00a47ad23508bcb6e5879432e9993a4ceead4
john-farina <clichecontent@gmail.com>
2022-11-10 22:39:11 -0500

    title

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,7 +3,7 @@ import Helmet from "react-helmet";
 function Head() {
   return (
     <Helmet>
-      <title>Hello wow</title>
+      <title>John Farina</title>
       <script
         src="https://kit.fontawesome.com/4c3322bec2.js"
         crossorigin="anonymous"
